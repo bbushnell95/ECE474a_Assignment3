7085dae77982d38fd4161d261a4ce9c73f25d6ec
bbushnell95 <bbushnell@email.arizona.edu>
2016-11-15 12:55:53 -0700

    Added code to both Node files

---
+++ hlsyn/src/Node.cpp
@@ -0,0 +1,84 @@
+/*
+Students: Brett Bushnell (Undergrad), Matt Dzurick (Grad)
+Date Create: 11/15/2016
+Assignment: 3
+File: Node.cpp
+Description: Node Class for hlsyn program
+*/
+
+#include "Node.h"
+
+Node::Node()
+{
+	nodeNum = 0;
+	visited = 'w';
+}
+
+Node::Node(int num, std::vector<DataType*> _inputs, std::vector<DataType*> _outputs)
+{
+	nodeNum = num;
+	_componentInputs = _inputs;
+	_componentOutputs = _outputs;
+	visited = 'w';
+
+}
+
+std::string Node::getOperation()
+{
+	return operation;
+}
+
+void Node::setOperation(std::string o)
+{
+	operation = o;
+}
+
+int Node::getNodeNum()
+{
+	return nodeNum;
+}
+
+void Node::setNodeNum(int num)
+{
+	nodeNum = num;
+}
+
+std::vector<DataType*> Node::getInputs()
+{
+	return _componentInputs;
+}
+
+void Node::setInputs(std::vector<DataType*> _inputs)
+{
+	_componentInputs = _inputs;
+}
+
+std::vector<DataType*> Node::getOutputs()
+{
+	return _componentOutputs;
+}
+
+void Node::setOutputs(std::vector<DataType*> _outputs)
+{
+	_componentOutputs = _outputs;
+}
+
+char Node::getVisted()
+{
+	return visited;
+}
+
+void Node::setVisted(char v)
+{
+	visited = v;
+}
+
+void Node::addInput(DataType* newInput)
+{
+	_componentInputs.push_back(newInput);
+}
+
+void Node::addOutput(DataType* newOutput)
+{
+	_componentOutputs.push_back(newOutput);
+}
--- hlsyn/src/Node.h
+++ hlsyn/src/Node.h
@@ -1,1 +1,77 @@
-#pragma once
+/*
+Students: Brett Bushnell (Undergrad), Matt Dzurick (Grad)
+Date Create: 11/15/2016
+Assignment: 3
+File: Node.h
+Description: Node Class for hlsyn program
+*/
+#ifndef NODE_H
+#define NODE_H
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<cstring>
+#include<algorithm>
+#include"DataType.h"
+
+class Node {
+private:
+	std::string operation;
+	int nodeNum;
+	std::vector<DataType*> _componentInputs;
+	std::vector<DataType*> _componentOutputs;
+	char visited;
+
+	///* Datapath Component inputs */
+
+
+	///* Datapath Component outputs */
+	//Wire outWire;
+	//Output outWire;
+
+	///* FOR INFORMATION ONLY
+	//	std::string validSymbols[13] = { "=","+" ,"-", "*", ">", "<","==", "?", ":", ">>", "<<", "/", "%"};
+	//*/
+	///*
+	//FUNCT___SYM___
+	//REG		=
+	//ADD		+
+	//SUB		-
+	//MUL		*
+	//COMP	<
+	//COMP	>
+	//COMP	==
+	//MUX2x1	? :
+	//SHR		>>
+	//SHL		<<
+	//DIV		/
+	//MOD		%
+	//INC		+1
+	//DEC		-1
+	//*/
+	//std::string component[12];
+
+public:
+	/*Constructors*/
+	Node();
+	Node(int num, std::vector<DataType*> _inputs, std::vector<DataType*> _outputs);
+
+	/*Methods*/
+	std::string getOperation();
+	void setOperation(std::string o);
+	int getNodeNum();
+	void setNodeNum(int num);
+	std::vector<DataType*> getInputs();
+	void setInputs(std::vector<DataType*> _inputs);
+	std::vector<DataType*> getOutputs();
+	void setOutputs(std::vector<DataType*> _outputs);
+	char getVisted();
+	void setVisted(char v);
+	void addInput(DataType* newInput);
+	void addOutput(DataType* newOutput);
+
+};
+
+#endif // NODE_H
